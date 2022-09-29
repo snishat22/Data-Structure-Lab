b784986d707e36cd8897ec76f94aebcc36a85701
snishat22 <114562188+snishat22@users.noreply.github.com>
2022-09-29 08:49:58 +0600

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//#include is a preprocessor directive that tells the preprocessor to include header files in the program
+//standard input-output stream.Header file contains definitions of objects like cin, cout
+using namespace std;
+
+//a namespace is a collection of related names or identifiers
+//The identifiers of the C++ standard library are defined in a namespace called std .
+//the members of the “std” namespace are cout, cin, endl, etc.
+int main(){
+
+    cout << "Hello"<<"  "<<"World"<<endl;
+
+return 0;
+}
